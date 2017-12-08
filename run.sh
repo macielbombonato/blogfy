e170bc9f54b3fe94f51830b06acf4c41810c24cf
Maciel Escudero Bombonato <maciel.bombonato@gmail.com>
2017-12-08 02:07:27 -0200

    Atualização dos scripts

--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
+echo '###############################'
+echo '# Parando container           #'
+echo '###############################'
+docker stop $(docker ps -a | grep blogfy | cut -c1-15)
+
+echo '#################################'
+echo '# Removendo container           #'
+echo '#################################'
+docker rm $(docker ps -a | grep blogfy | cut -c1-15)
+
 echo '#####################################'
 echo '#         running for tests         #'
 echo '#####################################'
 #docker container run -it macielbombonato/blogfy /bin/ash
 docker container run \
+    --name blogfy \
     -p 4000:4000 \
     -v ~/workspace/macielbombonato/jekyll/macielbombonato.github.io:/srv/jekyll \
     macielbombonato/blogfy
